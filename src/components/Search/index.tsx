6477da1e183fea88baa902ae262a021bd47a63c2
Vinicius Dias <vinicius-mdias@hotmail.com>
2020-09-03 10:51:28 -0300

    Create structure of Search component and add some styles in Pokemons page

---
+++ src/components/Search/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BsSearch } from "react-icons/bs";
+import { HiMicrophone } from "react-icons/hi";
+
+import './search.sass'
+
+
+const Search: React.FC = () => {
+  return (
+    <div className="search">
+      <BsSearch />
+      <input placeholder='Search' />
+      <HiMicrophone />
+    </div>
+  )
+}
+
+export default Search
--- src/pages/Pokemons/index.tsx
+++ src/pages/Pokemons/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import Container from '../../components/Container'
+import Search from '../../components/Search'
+
+import './pokemons.sass'
 
 const Login: React.FC = () => {
   return (
     <Container>
       <h1>Pokemon</h1>
+      <Search />
     </Container>
   )
 };
